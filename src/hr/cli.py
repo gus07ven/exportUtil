4181fcce208c48f3582d35c211a8bdbb8094eba2
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-11-04 19:03:38 -0600

    Add other files

--- src/hr/cli.py
+++ src/hr/cli.py
@@ -7,3 +7,23 @@ def create_parser():
     parser.add_argument('--format', default='json', choices=['json', 'csv'],
                         type=str.lower,help='The format of the export file')
     return parser
+
+
+def main():
+    import sys
+    from hr import export, users
+    from hr import users as u
+
+    args = create_parser().parse_args()
+    users = u.fetch_users()
+    if args.path:
+        file = open(args.path, 'w', newline='')
+    else:
+        file = sys.stdout
+
+    if args.format == 'json':
+        export.to_json_file(file, users)
+    else:
+        export.to_csv_file(file, users)
+
+
